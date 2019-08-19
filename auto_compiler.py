7ad3245cea7d34d333fa5133af45daae7b792e63
Илья Лукашонок <l.ilya@outlook.com>
2019-08-19 19:09:50 +0300

    Adding safety device

--- auto_compiler.py
+++ auto_compiler.py
@@ -4,18 +4,21 @@ import time
 
 VER = '0.7.0'
 
+def vr(line):
+    version = line.split("'")
+    return version[1]
+
 def take_version(file):
-    '''This def take version prom programm'''
+    '''This def take version from programm'''
     i = 1
     pattern = 'VERSION'
     while i: # Reading line by line
         line = file.readline()
         line = line.rstrip('\n')
         if re.search(pattern, line):   
-            exec(line) # Make a VERSION var
-            v = v + VERSION
+            version = vr(line)
             i = 0 #Stop loop
-    return v
+    return version
 NAME = input("Enter programm name: ")
 file_name = input("Enter file name: ")
 flags = input("Enter flags: ")
@@ -23,16 +26,22 @@ prev_version = '0.1.0'
 i = 1
 
 while i:
-    # time.sleep(30) # Delay
-    file = open(file_name, "r") # Open file
+    time.sleep(60) # Delay
+    while i:
+        try: # Safety device if we can't use a file
+            file = open(file_name, "r") # Open file
+        except:
+            time.sleep(30)
+        else:
+            i = 0
     version = take_version(file) # Take version
-    file = close() # Close file
+    file.close() # Close file
     if version != prev_version: # Checking for changes
         try: # Safety device if there is not this dir
             os.rmdir(dist) # Delete dir with prev compile file
         except:
             pass
-        new_name = name + version # Making a name
+        new_name = NAME + version # Making a name
         # Send command to compile to cmd
-        eval("os.system('pyinstaller -n' + new_name + flags + file_name)")
+        eval("os.system('pyinstaller -n' + new_name + flags + ' ' + file_name)")
         prev_version = version
