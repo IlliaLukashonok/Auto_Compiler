1a3d1ec4ccd3ceaa64b1edbdf43b71ce956f8d1e
Илья Лукашонок <l.ilya@outlook.com>
2019-08-19 19:24:51 +0300

    Start only as a programm

--- auto_compiler.py
+++ auto_compiler.py
@@ -19,29 +19,31 @@ def take_version(file):
             version = vr(line)
             i = 0 #Stop loop
     return version
-NAME = input("Enter programm name: ")
-file_name = input("Enter file name: ")
-flags = input("Enter flags: ")
-prev_version = '0.1.0'
-i = 1
 
-while i:
-    time.sleep(60) # Delay
+if __name__ == "__main__":
+    NAME = input("Enter programm name: ")
+    file_name = input("Enter file name: ")
+    flags = input("Enter flags: ")
+    prev_version = '0.1.0'
+    i = 1
+
     while i:
-        try: # Safety device if we can't use a file
-            file = open(file_name, "r") # Open file
-        except:
-            time.sleep(30)
-        else:
-            i = 0
-    version = take_version(file) # Take version
-    file.close() # Close file
-    if version != prev_version: # Checking for changes
-        try: # Safety device if there is not this dir
-            os.rmdir(dist) # Delete dir with prev compile file
-        except:
-            pass
-        new_name = NAME + ' ' + version # Making a name
-        # Send command to compile to cmd
-        eval("os.system('pyinstaller -n' + new_name + flags + ' ' + file_name)")
-        prev_version = version
+        time.sleep(60) # Delay
+        while i:
+            try: # Safety device if we can't use a file
+                file = open(file_name, "r") # Open file
+            except:
+                time.sleep(30)
+            else:
+                i = 0
+        version = take_version(file) # Take version
+        file.close() # Close file
+        if version != prev_version: # Checking for changes
+            try: # Safety device if there is not this dir
+                os.rmdir(dist) # Delete dir with prev compile file
+            except:
+                pass
+            new_name = NAME + ' ' + version # Making a name
+            # Send command to compile to cmd
+            eval("os.system('pyinstaller -n' + new_name + flags + ' ' + file_name)")
+            prev_version = version
