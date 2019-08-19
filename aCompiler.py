f15eef3d730665264ffeed8afe9f26d7c1ec074d
Illia Lukashonak <48157587+IlliaLukashonok@users.noreply.github.com>
2019-08-19 21:14:44 +0300

    Update aCompiler.py

--- aCompiler.py
+++ aCompiler.py
@@ -2,7 +2,7 @@ import os
 import re
 import time
 
-VER = '1.2.0'
+VER = '0.7.0'
 
 def vr(line):
     version = line.split("'")
@@ -19,31 +19,29 @@ def take_version(file):
             version = vr(line)
             i = 0 #Stop loop
     return version
+NAME = input("Enter programm name: ")
+file_name = input("Enter file name: ")
+flags = input("Enter flags: ")
+prev_version = '0.1.0'
+i = 1
 
-if __name__ == "__main__":
-    NAME = input("Enter programm name: ")
-    file_name = input("Enter file name: ")
-    flags = input("Enter flags: ")
-    prev_version = '0.1.0'
-    i = 1
-
+while i:
+    time.sleep(60) # Delay
     while i:
-        time.sleep(60) # Delay
-        while i:
-            try: # Safety device if we can't use a file
-                file = open(file_name, "r") # Open file
-            except:
-                time.sleep(30)
-            else:
-                i = 0
-        version = take_version(file) # Take version
-        file.close() # Close file
-        if version != prev_version: # Checking for changes
-            try: # Safety device if there is not this dir
-                os.rmdir(dist) # Delete dir with prev compile file
-            except:
-                pass
-            new_name = NAME + ' ' + version # Making a name
-            # Send command to compile to cmd
-            eval("os.system('pyinstaller ' + flags + ' -n ' + new_name + ' ' + file_name)")
-            prev_version = version
+        try: # Safety device if we can't use a file
+            file = open(file_name, "r") # Open file
+        except:
+            time.sleep(30)
+        else:
+            i = 0
+    version = take_version(file) # Take version
+    file.close() # Close file
+    if version != prev_version: # Checking for changes
+        try: # Safety device if there is not this dir
+            os.rmdir('dist') # Delete dir with prev compile file
+        except:
+            pass
+        new_name = NAME + version # Making a name
+        # Send command to compile to cmd
+        eval("os.system('pyinstaller -n' + new_name + ' ' + flags + ' ' + file_name)")
+        prev_version = version
